f18bf8cefc8f7a64be3595e02e202f6bbfb2d369
ShinSocks <astorabaguette@gmail.com>
2023-02-10 17:57:05 -0800

    create pages

--- src/App.js
+++ src/App.js
@@ -1,7 +1,23 @@
+import {BrowserRouter as Router, Routes, Route} from "react-router-dom";
+import Home from "./pages/Home";
+import AlumniPage from "./pages/AlumniPage";
+import SignIn from "./pages/SignIn";
+import SignUp from "./pages/SignUp";
+import ForgotPassword from "./pages/ForgotPassword";
+import Schools from "./pages/Schools";
 
 function App() {
   return (
-    <h1 className='text-2xl'>Hellow World</h1>
+    <Router>
+      <Routes>
+        <Route path="/" element = {<Home/>}/> 
+        <Route path="/alumni-page" element = {<AlumniPage/>}/> 
+        <Route path="/sign-in" element = {<SignIn/>}/> 
+        <Route path="/sign-up" element = {<SignUp/>}/> 
+        <Route path="/forgot-password" element = {<ForgotPassword/>}/> 
+        <Route path="/schools" element = {<Schools/>}/> 
+      </Routes>
+    </Router>
   );
 }
 
---
+++ src/pages/AlumniPage.jsx
@@ -0,0 +1,7 @@
+
+
+export default function AlumniPage() {
+  return (
+    <div>AlumniPage</div>
+  )
+}
