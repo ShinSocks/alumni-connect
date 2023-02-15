f3857abfca76d55e821e2aab86a7dbdc1acec561
ShinSocks <astorabaguette@gmail.com>
2023-02-14 23:15:22 -0800

    signin

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -12,13 +12,16 @@ export default function Header() {
                 
             </div>
             <div>
-                <form>
+                <form className = "px-10">
                     <input type = "text" className = "bg-slate-500 rounded-sm placeholder-white px-3 w-96 h-9" placeholder = "Search"></input>
-                    <input type = "submit"></input>
+                    <input type = "submit" className = "text-slate-50 ml-3 cursor-pointer hover:text-slate-500" ></input>
                 </form>
             </div>
-            <div className = "text-slate-50">
-                login
+            <div className = "text-slate-50 hover:text-slate-500 cursor-pointer" onClick = {()=>navigate("/sign-in")}>
+                <ul className="flex space-x-1">
+                    <li><img src={require('../images/lockIcon.png')} alt = "lock icon" className = "h-7"/></li>
+                    <li>Login</li>
+                </ul>
             </div>
         </header>
     </div>
--- src/pages/SignIn.jsx
+++ src/pages/SignIn.jsx
@@ -1,7 +1,22 @@
-
-
 export default function SignIn() {
   return (
-    <div>SignIn</div>
+    <div className = "bg-slate-500 w-[27%] m-auto mt-48">
+      <div>
+        <img src = {require('../images/UCC.png')} alt="UCC Logo" className = "h-16 m-auto py-2"/>
+      </div>
+      <div className = "bg-slate-400 h-48 pl-2 pt-3">
+        Email Address
+        <form>
+          <ul>
+            <li><input type = "text" placeholder="Email"></input></li>
+            <li><input type = "text" placeholder="Password"></input></li>
+            <li><input type = "submit"></input></li>
+          </ul>
+          
+          
+          
+        </form>
+      </div>
+    </div>
   )
 }
